932f9202a1970c51659ad7353bb3a5a6eb2a690c
Hitendra <Hitendra@Hitendra-PC>
2020-06-14 19:00:28 +0530

    first commit

---
+++ ExtentDemo/src/main/java/com/basePackage/BaseClass.java
@@ -0,0 +1,75 @@
+package com.basePackage;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import com.extentManager.ExtentManager;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	public static WebDriver driver;
+
+	@BeforeSuite
+	public void BeforeSuite() {
+		ExtentManager.setExtent();
+	}
+	
+	@AfterSuite
+	public void AfterSuite() {
+		ExtentManager.endReport();
+	}
+	
+	@BeforeMethod
+	public void setup() {
+		if (System.getProperty("browser").equalsIgnoreCase("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (System.getProperty("browser").equalsIgnoreCase("Firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (System.getProperty("browser").equalsIgnoreCase("IE")) {
+			WebDriverManager.iedriver().setup();
+			driver = new InternetExplorerDriver();
+
+		}
+		driver.manage().window().maximize();
+	}
+	
+	@AfterMethod
+	public void tearDown() throws IOException {
+		driver.close();
+	} 
+	
+	public static String screenShot(WebDriver driver,String filename) {
+		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+		File source = takesScreenshot.getScreenshotAs(OutputType.FILE);
+		String destination = System.getProperty("user.dir")+"\\ScreenShot\\"+filename+"_"+dateName+".png";
+		File finalDestination= new File(destination);
+		try {
+			FileUtils.copyFile(source, finalDestination);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.getMessage();
+		}
+		return destination;
+	}
+	
+	public static String getCurrentTime() {  
+	    String currentDate = new SimpleDateFormat("yyyy-MM-dd-hhmmss").format(new Date());  
+	    return currentDate;  
+	}  
+}
---
+++ ExtentDemo/src/test/java/com/test/OrangeHRMTest.java
@@ -0,0 +1,38 @@
+package com.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.basePackage.BaseClass;
+import com.extentManager.ExtentManager;
+
+public class OrangeHRMTest extends BaseClass {
+	
+	@Test
+	public void loginPageTest() {
+		
+		WebElement imgElement=driver.findElement(By.xpath("//*[@id=\"divLogo\"]/img"));
+		Assert.assertTrue(imgElement.isDisplayed());
+	}
+	@Test
+	public void loginTest() {
+		driver.findElement(By.id("txtUsername")).sendKeys("admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
+        String actualTitle=driver.getTitle();
+        String expectedTitle="OrangeHRM1";
+        Assert.assertEquals(actualTitle, expectedTitle);
+	}
+	@Test
+	public void sampleCase() {
+		ExtentManager.test.createNode("Validation1");
+		Assert.assertTrue(true);
+		ExtentManager.test.createNode("Validation2");
+		Assert.assertTrue(true);
+		ExtentManager.test.createNode("Validation3");
+		Assert.assertTrue(true);
+		ExtentManager.test.createNode("Validation4");
+		Assert.assertTrue(true);
+	}
+}
