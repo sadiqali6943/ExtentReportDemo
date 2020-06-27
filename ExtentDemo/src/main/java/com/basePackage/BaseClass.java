bfa8c9a1545e90676e2d3529ee0c24831ce15178
Hitendra <Hitendra@Hitendra-PC>
2020-06-27 12:04:24 +0530

    test

--- ExtentDemo/src/main/java/com/basePackage/BaseClass.java
+++ ExtentDemo/src/main/java/com/basePackage/BaseClass.java
@@ -41,9 +41,20 @@ public class BaseClass {
 		WebDriverManager.chromedriver().setup();
 		
 		ChromeOptions chromeOptions= new ChromeOptions();
-		chromeOptions.addArguments("headless");
-		chromeOptions.addArguments("window-size=1200x400");
+		
+		 chromeOptions.addArguments("disable-infobars");
+	     chromeOptions.addArguments("--incognito");
+	     chromeOptions.addArguments("--disable-gpu");
+	     chromeOptions.addArguments("--no-sandbox");
+	     chromeOptions.addArguments("--allow-insecure-localhost");
+	     chromeOptions.addArguments("--headless");
+	     chromeOptions.addArguments("--window-size=1980,1080");
+		
+		//chromeOptions.addArguments("headless");
+		//chromeOptions.addArguments("window-size=1200x400");
 	    driver = new ChromeDriver(chromeOptions);
+	   
+	   
 	    
 		driver.manage().window().maximize();
 		driver.get("https://opensource-demo.orangehrmlive.com/index.php/");
